b5e7ddd81da456b4d027de6c3d00c2924ce1bfa1
Michelle Vettese <48606819+mchlv@users.noreply.github.com>
2020-05-08 19:43:31 -0700

    auto set theme

--- src/App.js
+++ src/App.js
@@ -26,21 +26,44 @@ import {
 // create a class 
 export default class App extends Component {
 
-  
-
   constructor(props) {
     super(props);
-
+    
     this.state = {
-        dark: false
+        dark: false,
+        checked: false
     };
 
     this.changeTheme = this.changeTheme.bind(this);
   }
 
+
+  // on checkbox toggle, set the dark state to true which is changing the theme from theme--default to theme--dark, and set the checkbox state to true which is setting the value of the checkbox to 'checked' and putting the toggle button in the correct position
   changeTheme() {
-      this.setState({ dark: !this.state.dark });
+      this.setState({ dark: !this.state.dark, checked: !this.state.checked });
   }
+
+
+  // on DOM load, if night time, set theme to dark and checkbox to checked
+  componentDidMount() {
+    const hour = new Date().getHours();
+    const night = hour < 6 || hour >= 19;
+
+    if (night) {
+      this.setState({ dark: !this.state.dark, checked: !this.state.checked });
+      console.log(this.state.checked)
+    } 
+    // else {
+    //   this.setState({ dark: this.state.dark, checked: !this.state.checked });
+    // }
+  }
+
+  componentDidUpdate() {
+    console.log("it updated");
+
+  }
+
+  
 
   render() {
     return (
@@ -56,10 +79,10 @@ export default class App extends Component {
                     <Link to="/"><img src={logo} alt="Michelle Vettese Design & Development Logo" className="logo" /></Link>
 
                     {/* theme toggle  */}
-                    <div className="toggle-button-cover" onClick={this.changeTheme} >
+                    <div className="toggle-button-cover" onChange={this.changeTheme} >
                       <div className="button-cover">
                         <div className="button r" id="button-2">
-                          <input type="checkbox" className="checkbox" />
+                          <input type="checkbox" className="checkbox" checked={this.state.checked} />
                           <div className="knobs"></div>
                           <div className="layer"></div>
                         </div>
